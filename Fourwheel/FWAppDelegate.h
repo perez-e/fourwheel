80445bc91d3a4efeb2f2664f3bebc72b311b0515
Eric Perez <ericricardoperez@gmail.com>
2014-05-27 11:52:17 -0700

    Initial Commit

---
+++ Fourwheel/FWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FWAppDelegate.h
+//  Fourwheel
+//
+//  Created by Eric Perez on 5/27/14.
+//  Copyright (c) 2014 Fourwheel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fourwheel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fourwheel
+//
+//  Created by Eric Perez on 5/27/14.
+//  Copyright (c) 2014 Fourwheel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FWAppDelegate class]));
+    }
+}
