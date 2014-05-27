b30360b0b88dc1b813741b89839240d237a26555
Eric Perez <ericricardoperez@gmail.com>
2014-05-27 13:24:17 -0700

    main view controllers and xibs

---
+++ Fourwheel/FWCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWCameraViewController.h
+//  Fourwheel
+//
+//  Created by Eric Perez on 5/27/14.
+//  Copyright (c) 2014 Fourwheel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWCameraViewController : UIViewController
+
+@end
